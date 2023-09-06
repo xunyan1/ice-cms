e6c4a9a4933fc080f4b84634ff2f61adced83d07
zyt <1910927210@qq.com>
2023-09-06 15:18:23 +0800

    添加几个vo文件

---
+++ IceWk-ment/src/main/java/com/cssl/icewkment/commin/vo/ArticleStatusVo.java
@@ -0,0 +1,9 @@
+package com.cssl.icewkment.commin.vo;
+
+import lombok.Data;
+
+@Data
+public class ArticleStatusVo {
+    private Integer articleStatus;
+    private Integer id;
+}
---
+++ IceWk-ment/src/main/java/com/cssl/icewkment/commin/vo/ClassNameVo.java
@@ -0,0 +1,8 @@
+package com.cssl.icewkment.commin.vo;
+
+import lombok.Data;
+
+@Data
+public class ClassNameVo {
+    private String name;
+}
